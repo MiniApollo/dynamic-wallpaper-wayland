d5c146343016bda8b95a3a525dafdb2cc381e376
MiniApollo <surmannmark@gmail.com>
2024-01-28 16:59:32 +0100

    wallpaper.sh: Use env shell, Prettier comment structure

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,23 +1,22 @@
-#!/bin/sh 
+#!/usr/bin/env bash
 
-## Author  : SMark5
-#Inspired by adi1090x/dynamic-wallpaper 
+## Author: MiniApollo
+## Inspired by adi1090x/dynamic-wallpaper
 
 ## Dynamic Wallpaper : Set wallpapers according to current time.
 ## Created to work better with job schedulers (cron)
 
-#change the DIR variable accordingly where you want to place it
+## Change the DIR variable accordingly where you want to place it
 DIR="$HOME/SystemFiles/dynamic-wallpaper-wayland/images/firewatch/"
 HOUR=$(($(date +%k)/1))
 
-#set background
-#wayland
+### Set background
+## Wayland
 swaybg -o \* -i $DIR$HOUR.png -m fill &
 sleep 1
-# kill all older processes of swaybg
+## Kill all older processes of swaybg
 kill $(ps -eo pid,etime,comm | grep 'swaybg' | awk '{ print $1 }' | head -n -1)
 
-#xorg
-#if you want to use xorg just uncomment the line bellow
-#and comment the wayland lines above
+## Xorg
+## If you want to use xorg just uncomment the line bellow and comment the wayland lines above
 #feh --bg-fill $DIR$HOUR.png
